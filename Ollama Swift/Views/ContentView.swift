d24c56158c198efe850ffc514577dedbcac04cab
TcDrozd <trcharlesd@gmail.com>
2025-02-04 16:08:11 -0500

    Continued customizing, working on appearance first because I feel like working backwards

--- Ollama Swift/Views/ContentView.swift
+++ Ollama Swift/Views/ContentView.swift
@@ -9,10 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     @StateObject var ollamaController = OllamaController()
+    
     var body: some View {
-        NavigationStack{
-            ChatView()
+        NavigationView {
+            List {
+                NavigationLink("Chat", destination: ChatView())
+                NavigationLink("Settings", destination: SettingsView())
+            }
+            .listStyle(SidebarListStyle()) // Ensures the list looks like a sidebar
+            
+            Text("Select an option")
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
         }
+        .navigationTitle("Ollama Swift")
     }
 }
 
